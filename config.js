3b5c6021f817065b52e1f582601011ba110a9e68
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-11-24 09:27:09 -0800

    [skip ci] adding changes to arezzo

--- config.js
+++ config.js
@@ -47,9 +47,31 @@ const routes = [
   {
     name: 'Order Online',
     isPrimaryCalledToAction: false,
-    url: 'https://order.toasttab.com/online/arezzo-ristorante-5-riverside-ave',
+    // url: 'https://order.toasttab.com/online/arezzo-ristorante-5-riverside-ave',
     seoPageTitle: '',
     tag: 'discover',
+    children: [
+      {
+        name: 'Toast',
+        isPrimaryCalledToAction: false,
+        url: 'https://order.toasttab.com/online/arezzo-ristorante-5-riverside-ave',
+      },
+      {
+        name: 'Grubhub',
+        isPrimaryCalledToAction: false,
+        url: 'https://www.grubhub.com/restaurant/arezzo-ristorante-5-riverside-ave-westport/7143776',
+      },
+      {
+        name: 'Door Dash',
+        isPrimaryCalledToAction: false,
+        url: 'https://www.doordash.com/store/arezzo-ristorante-westport-26218604',
+      },
+      {
+        name: 'UberEats',
+        isPrimaryCalledToAction: false,
+        url: 'https://www.ubereats.com/store/arezzo-ristorante/9GJldc5PW5iwr_DfzSj8Ow?diningMode=DELIVERY',
+      },
+    ],
   },
 
   {
